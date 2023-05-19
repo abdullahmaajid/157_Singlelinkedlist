d3eb6af1eaafc60eaafc7a37743235f23cde138b
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-05-19 11:08:04 +0700

    if (START == NULL || nim << START-> noMhs) {   if (START != NULL && nim == START->noMhs) {    cout << "NIM sudah ada" << endl;    return;   }  }

--- 157_Singlelinkedlist/157_Singlelinkedlist.cpp
+++ 157_Singlelinkedlist/157_Singlelinkedlist.cpp
@@ -21,4 +21,16 @@ void addNode() {
 	nodeBaru-> noMhs = nim;
 	nodeBaru-> name = nama;
 
+	if (START == NULL || nim << START-> noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+	}
+
+	Node* current = START;
+	Node*
+
+
+
 
