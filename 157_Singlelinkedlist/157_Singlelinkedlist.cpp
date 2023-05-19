bdec1a94b4eb0684fbbe93054e6886094d6ef6e7
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-05-19 11:14:17 +0700

    while (current != NULL && nim > current->noMhs)

--- 157_Singlelinkedlist/157_Singlelinkedlist.cpp
+++ 157_Singlelinkedlist/157_Singlelinkedlist.cpp
@@ -46,7 +46,10 @@ void addNode() {
 bool searchNode(int nim, Node* current, Node* previous) {
 	previous = START;
 	current = START;
-	
+	while (current != NULL && nim > current->noMhs)
+	{
+
+	}
 }
 
 
