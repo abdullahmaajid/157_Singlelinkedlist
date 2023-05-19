fd31a71425379e1c37395d826bff9671b69c950c
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-05-19 11:19:57 +0700

    bool deleteNode(int nim) {  Node* current = START;  Node* previous = START;  if (searchNode(nim, previous, current) == false)   return false;  previous->next = current->next   return false;  if (current == START)   START = current->next;  return true; }

--- 157_Singlelinkedlist/157_Singlelinkedlist.cpp
+++ 157_Singlelinkedlist/157_Singlelinkedlist.cpp
@@ -70,8 +70,18 @@ bool searchNode(int nim, Node* current, Node* previous) {
 }
 
 bool deleteNode(int nim) {
-
+	Node* current = START;
+	Node* previous = START;
+	if (searchNode(nim, previous, current) == false)
+		return false;
+	previous->next = current->next
+		return false;
+	if (current == START)
+		START = current->next;
+	return true;
 }
+
+
 
 
 
